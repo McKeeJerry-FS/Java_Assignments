01c8695b0ac1b48ee6903dff9f0110f9fb34033f
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-07-22 22:21:14 -0400

    Add assignment folder to repo
    added athe Intro_to_Java assignmnet folder to Repo

---
+++ Intro_To_Java/src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public static void main (String[] args){
+        String message = "Hello World";
+        int num1 = 1982;
+        int thisYear = 2021;
+        String message2 = "Welcome to the world of Java!";
+        int covidYear = 2020;
+        System.out.println(message);
+        System.out.println(message2);
+
+        System.out.println(covidYear + " is the year of the worldwide pandemic. It wasn't until spring of " + thisYear + " that things began to get back to normal.");
+    }
+
+}
